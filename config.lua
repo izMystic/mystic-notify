f860ec47c6f021ed2e75c53e00fc5705e2042e65
izMystic <mystic@izmystic.dev>
2021-12-01 12:40:18 -0500

    bruh moment

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.Notify {
+Config.Notify = {
     stack = false,                      -- Allow notifications to stack
     position = 'top-right',             -- top-left | top-right | bottom-left | bottom-right | top | bottom | left | right | center
     progress = true                     -- Display Progress Bar
