2b56d5e2e5600c124c19a9aa0d0abdadb489d2fe
izMystic <mystic@izmystic.dev>
2021-12-01 12:21:55 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 Config.Notify {
-    stack = false,
-    position = 'top-right',
-    progress = true
+    stack = false,                      -- Allow notifications to stack
+    position = 'top-right',             -- top-left | top-right | bottom-left | bottom-right | top | bottom | left | right | center
+    progress = true                     -- Display Progress Bar
 }
